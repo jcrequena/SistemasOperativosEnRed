e9e7aba0e81f9168649c21c8e5d77c7ffc1fc7d8
Juan Carlos <jrequenav@iesmiralcamp.org>
2016-10-08 19:46:47 +0200

    Update alta_Equipos.ps1

--- Powershell/alta_Equipos.ps1
+++ Powershell/alta_Equipos.ps1
@@ -1,1 +1,19 @@
-v
+#alta_Equipos.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 la extensión y parámetro 3 la ruta del fichero csv
+
+param($a,$b,$c)
+#DC=smr,DC=local
+$dc="dc="+$a+",dc="+$b
+$equiposCsv=$c
+#
+#Creación de los grupos a partir de un fichero csv
+#
+#Lee el fichero grupos.csv
+$fichero= import-csv -Path $equiposCsv
+$Class = "Computer"
+$ADSI = [ADSI]"LDAP://cn=computers,$dc"
+foreach($linea in $fichero)
+{
+    $Name = "CN="+$linea.Equipo
+    $Computer = $ADSI.create($Class, $Name)
+    $Computer.setInfo()
+}
