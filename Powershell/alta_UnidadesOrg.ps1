c0aeb5039d419b1a2bc7f5d60eecf3925bfe7a09
Juan Carlos <jrequenav@iesmiralcamp.org>
2016-10-08 19:00:16 +0200

    Update alta_UnidadesOrg.ps1

--- Powershell/alta_UnidadesOrg.ps1
+++ Powershell/alta_UnidadesOrg.ps1
@@ -1,10 +1,13 @@
+#alta_UnidadesOrg.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 la extensión y parámetro 3 la ruta del fichero csv
 #
 #Creación de las unidades organizativas
 #
-param($a,$b)
+param($a,$b,$c)
+#DC=smr,DC=local
 $dc="dc="+$a+",dc="+$b
+$uoCsv=$c
 
-$fichero = import-csv -Path unidades_org.csv
+$fichero = import-csv -Path $uoCsv
 $ADSI = [ADSI]"LDAP://$dc"
 $Class = "OrganizationalUnit"
 foreach($linea in $fichero)
