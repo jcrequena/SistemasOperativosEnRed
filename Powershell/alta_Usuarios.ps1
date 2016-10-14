f7c238ad43972817a817d1b6490bceafaa9abbb8
Juan Carlos <jrequenav@iesmiralcamp.org>
2016-10-14 09:29:55 +0200

    Update alta_Usuarios.ps1

--- Powershell/alta_Usuarios.ps1
+++ Powershell/alta_Usuarios.ps1
@@ -21,31 +21,25 @@ foreach($linea in $fichero)
 	$User = $ADSI.create($Class,$cnuser)
 
 	$User.put("UserPrincipalName", $nombre)
-	$User.setInfo()
-
 	$User.put("SamAccountName", $nombre)
-	$User.setInfo()
-
 	$User.put("givenName", $($linea.Nombre))
-	$User.setInfo()
-
 	$User.put("sn", $($linea.PrimerApellido+" "+$linea.SegundoApellido))
-	$User.setInfo()
-  
 	$User.put("displayName", $($linea.Nombre+" "+$linea.PrimerApellido+" "+$linea.SegundoApellido))
-	$User.setInfo()
-  
 	$User.put("mail", $($nombre+"@"+$a+"."+$b))
 	$User.setInfo()
-
+	#
+	#Lo último que hacemos es: Establecer el Password y si el user está habilitado o no.
+	#
 	$User.SetPassword($($linea.DNI))
 	$User.psbase.invokeset("AccountDisabled", "False")
 	$User.setInfo()
-
-	$cngrupo="cn="+$linea.Nivel
-	$uo_users="cn=users"
-	$grupoActual = [ADSI]"LDAP://$cngrupo,$uo_users,$dc"
-	$grupoActual.Add("LDAP://$cnuser,$ou,$dc")
-	$grupoActual.SetInfo()
+        
+	#Membresía de grupo: Con esta parte de código, establecemos los grupos de los cuales será
+	#miembro el usuario creado
+	$cngrupo="cn="+$linea.Nivel   #Nombre del grupo
+	$uo_users="cn=users"    #Ruta donde está ubicado el grupo, en este caso, está ubicado en el contenedor Users
+	$grupoActual = [ADSI]"LDAP://$cngrupo,$uo_users,$dc" #ADSI: LDAP://Nombre_grupo,ruta_del_grupo,controlador_dominio
+	$grupoActual.Add("LDAP://$cnuser,$ou,$dc") # Añadimos al grupo (membresía) el usuario creado haciendo uso del protocolo LDAP
+	$grupoActual.SetInfo() #Guardamos en la BD la información
 
 }
