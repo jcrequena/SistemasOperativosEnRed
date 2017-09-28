e8ed25d2fe1cd41bd8d6a9b1fe1cb31bed6ec356
Juan Carlos <jrequenav@iesmiralcamp.org>
2017-09-28 11:36:04 +0200

    Create PromocionCD-Core.ps1

---
+++ UD2/PromocionCD-Core.ps1
@@ -0,0 +1,13 @@
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012R2" `
+-DomainName "DC2012R2.ES" `
+-DomainNetbiosName "DC2012R2" `
+-ForestMode "Win2012R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
