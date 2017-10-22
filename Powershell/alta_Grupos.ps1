7cb6e6b29d8c2f0c08853e744aa7826868f3856f
Juan Carlos <jrequenav@iesmiralcamp.org>
2017-10-22 18:58:56 +0200

    Update alta_Grupos.ps1

--- Powershell/alta_Grupos.ps1
+++ Powershell/alta_Grupos.ps1
@@ -1,4 +1,4 @@
-#alta_Grupos.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 la extensión y parámetro 3 la ruta del fichero csv
+s#alta_Grupos.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 el sufijo del dominio y parámetro 3 la ruta del fichero csv
 
 param($a,$b,$c)
 #DC=smr,DC=local
@@ -8,12 +8,9 @@ $gruposCsv=$c
 #Creación de los grupos a partir de un fichero csv
 #
 #Lee el fichero grupos.csv
-$fichero= import-csv -Path $gruposCsv
-$Class = "Group"
-$ADSI = [ADSI]"LDAP://cn=users,$dc"
+$fichero = import-csv -Path $ficheroCsvUO -delimiter :
 foreach($linea in $fichero)
 {
-    $Name = "CN="+$linea.Grupo
-    $Group = $ADSI.create($Class, $Name)
-    $Group.setInfo()
+	$rutaObject=$linea.Path+","+$dc
+	New-ADGroup -Description:$linea.Descripcion -GroupCategory:"Security" -GroupScope:"Global" -Name:$linea.Nombre -Path:$rutaObject -SamAccountName:$linea.Nombre		
 }
