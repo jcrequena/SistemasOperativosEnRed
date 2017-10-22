72e0890ad832207027e43a772621fe3714cfc995
Juan Carlos <jrequenav@iesmiralcamp.org>
2017-10-22 18:17:26 +0200

    Update alta_Equipos.ps1

--- Powershell/alta_Equipos.ps1
+++ Powershell/alta_Equipos.ps1
@@ -1,4 +1,5 @@
-#alta_Equipos.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 la extensión y parámetro 3 la ruta del fichero csv
+# alta_Equipos.ps1 : Parámetro 1 el dc (nombre netbios del dominio) parámetro 2 el sufijo del dominio y 
+# parámetro 3 la ruta del fichero csv con los equipos
 
 param($a,$b,$c)
 #DC=smr,DC=local
@@ -7,13 +8,15 @@ $equiposCsv=$c
 #
 #Creación de los grupos a partir de un fichero csv
 #
-#Lee el fichero grupos.csv
-$fichero= import-csv -Path $equiposCsv
-$Class = "Computer"
-$ADSI = [ADSI]"LDAP://cn=computers,$dc"
+#Lee el fichero grupos.csv. El carácter delimitador de columna es :
+$fichero= import-csv -Path $equiposCsv -delimiter ":"
+
 foreach($linea in $fichero)
 {
-    $Name = "CN="+$linea.Equipo
-    $Computer = $ADSI.create($Class, $Name)
-    $Computer.setInfo()
+	$pathObject=$linea.Ruta+","+$dc	 
+	New-ADComputer -Enabled:$true -Name:$linea.Equipo -Path:$pathObject -SamAccountName:$linea.Equipo
 }
+
+write-Host ""
+write-Host "Se han creado los equipos" -Fore green
+write-Host "" 
