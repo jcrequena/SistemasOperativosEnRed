c4cfe748458d1c5a3c0a0eeda802e1058b024d9c
Juan Carlos <jrequenav@iesmiralcamp.org>
2018-10-11 17:42:31 +0100

    Update menu-Powershell.ps1

--- UD2/menu-Powershell.ps1
+++ UD2/menu-Powershell.ps1
@@ -1,6 +1,21 @@
 #Fuente: https://gallery.technet.microsoft.com/scriptcenter/Menu-simple-en-PowerShell-95e1f923
 
+#Paso de parámetros al ejecutar el script en la consola
+#Este script recoje 2 parámetros en la llamada del mismo en la consola. 
+#La ejecución del script será: ./menu.ps1 param1 param2
+#Los parámetros son opcionales, si la llamada es:./menu.ps1 los valores de $Param1 y $Param2, será la cadena de texto vacía.
+Param(
+  [string]$Param1,
+  [string]$Param2
+)
+Write-Host "Los parámetros son:"$Param1 " " $Param2
+pause
+
 #Función que nos muestra un menú por pantalla con 3 opciones y una última para salir del mismo
+# La función “mostrarMenu”, puede tomar como parámetro un título y devolverá por pantalla 
+# "================ $Titulo================" , donde $Titulo será el título pasado por parámetro.
+#Si no se le pasa un parámetro, por defecto $Titulo contendrá la cadena 'Selección de opciones'  
+#https://technet.microsoft.com/es-es/library/jj554301.aspx
 function mostrarMenu 
 { 
      param ( 
