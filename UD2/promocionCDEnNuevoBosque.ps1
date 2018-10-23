3ffd5d65e0a5a95e046757177c0922d4ff793768
Juan Carlos <jrequenav@iesmiralcamp.org>
2018-10-23 19:55:54 +0200

    Update promocionCDEnNuevoBosque.ps1

--- UD2/promocionCDEnNuevoBosque.ps1
+++ UD2/promocionCDEnNuevoBosque.ps1
@@ -8,6 +8,8 @@
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools 
 
 # cmdlet de promoción a CD en un nuevo bosque
-Import-Module ADDSDeployment Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "smr.local" -DomainNetbiosName "smr" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
-
-
+if (!(Get-Module -Name ADDSDeployment)) #Se comprueba si se tiene cargado el módulo
+{
+  Import-Module ADDSDeployment #Se carga el módulo
+}
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "smr.local" -DomainNetbiosName "smr" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
