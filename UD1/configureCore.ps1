47479aaa61534697ada33551f08337f6660bf121
Juan Carlos <jcrequena.fp@gmail.com>
2021-09-24 16:44:00 +0200

    Update configureCore.ps1

--- UD1/configureCore.ps1
+++ UD1/configureCore.ps1
@@ -1,6 +1,15 @@
+#CmdLets RED
+
 #Para consultar la información de nuestra interfaz de red
 Get-NetIPConfiguration
 #Para obtener el adaptador de red
 Get-NetAdapter
+
 #Para configurar la direccion IP (suponiendo que nuestro adaptador de red es el 2)
 New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.20 -PrefixLength 24 -DefaultGateway 192.168.1.1
+#Para configurar el DNS (suponiendo que nuestro adaptador de red es el 2)
+Set-DnsClientServerAddress -InterfaceIndex 2 -ServerAddresses 192.168.1.1
+
+
+#Cambiar el nombre del equipo (el nuevo nombre será orion)
+Rename-Computer -NewName orion
