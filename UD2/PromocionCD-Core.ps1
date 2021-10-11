0aab3e53b2f5ccfbec4068392e69dbfd38c78bba
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-11 20:42:58 +0200

    Update PromocionCD-Core.ps1

--- UD2/PromocionCD-Core.ps1
+++ UD2/PromocionCD-Core.ps1
@@ -11,3 +11,14 @@ Install-ADDSForest `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
+
+#Nota:
+#https://docs.microsoft.com/en-us/powershell/module/addsdeployment/install-addsforest?view=windowsserver2019-ps
+#Domain Mode y -ForestMode
+#Windows Server 2003: 2 or Win2003
+#Windows Server 2008: 3 or Win2008
+#Windows Server 2008 R2: 4 or Win2008R2
+#Windows Server 2012: 5 or Win2012
+#Windows Server 2012 R2: 6 or Win2012R2
+#Windows Server 2016: 7 or WinThreshold
+
