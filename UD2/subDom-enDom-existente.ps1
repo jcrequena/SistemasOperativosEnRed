b00db9ba45cdf482a71b729431f2887e18118931
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-11 20:44:28 +0200

    Update subDom-enDom-existente.ps1

--- UD2/subDom-enDom-existente.ps1
+++ UD2/subDom-enDom-existente.ps1
@@ -9,5 +9,16 @@ if (!(Get-Module -Name ADDSDeployment)) #Se comprueba si se tiene cargado el mó
 {
   Import-Module ADDSDeployment #Se carga el módulo
 }
-Install-ADDSDomain -NoGlobalCatalog:$false -CreateDnsDelegation:$true -Credential (Get-Credential) -DatabasePath "C:\Windows\NTDS" -DomainMode "Default" -DomainType "ChildDomain" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NewDomainName "sor" -NewDomainNetbiosName "SOR" -ParentDomainName "smr.local" -NoRebootOnCompletion:$false -SiteName "Default-First-Site-Name" -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+Install-ADDSDomain -NoGlobalCatalog:$false -CreateDnsDelegation:$true -Credential (Get-Credential) -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainType "ChildDomain" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NewDomainName "sor" -NewDomainNetbiosName "SOR" -ParentDomainName "smr.local" -NoRebootOnCompletion:$false -SiteName "Default-First-Site-Name" -SysvolPath "C:\Windows\SYSVOL" -Force:$true
 #-Force:$true fuerza la instalación sin preguntar al usuario.
+
+#Nota:
+#https://docs.microsoft.com/en-us/powershell/module/addsdeployment/install-addsforest?view=windowsserver2019-ps
+#Domain Mode y -ForestMode
+#Windows Server 2003: 2 or Win2003
+#Windows Server 2008: 3 or Win2008
+#Windows Server 2008 R2: 4 or Win2008R2
+#Windows Server 2012: 5 or Win2012
+#Windows Server 2012 R2: 6 or Win2012R2
+#Windows Server 2016: 7 or WinThreshold
+
