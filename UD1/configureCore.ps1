c113ffd2a30eb85cb8da41f7b0f47ebde4498ca5
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-11 20:35:54 +0200

    Update configureCore.ps1

--- UD1/configureCore.ps1
+++ UD1/configureCore.ps1
@@ -10,6 +10,8 @@ New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.20 -PrefixLength 24 -Def
 #Para configurar el DNS (suponiendo que nuestro adaptador de red es el 2)
 Set-DnsClientServerAddress -InterfaceIndex 2 -ServerAddresses 192.168.1.1
 
+#NOTA:Si se tiene sólo 1 interfaz de red el comando se puede simplificar
+New-NetIPAddress –IPAddress 192.168.1.20 -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 
 #Cambiar el nombre del equipo (el nuevo nombre será orion)
 Rename-Computer -NewName orion
