5e1b307f84ab2d1010d986efbaa93a1184e8c6b7
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-09 16:21:33 +0100

    Update permisosNTFS.ps1

--- UD4/dirPersonales/permisosNTFS.ps1
+++ UD4/dirPersonales/permisosNTFS.ps1
@@ -1,14 +1,13 @@
-
-
 #usuario juancarlos.requena 
 $cuenta="juancarlos.requena"
 
 #Creamos Permisos Totales para el usuario
 New-Item -Path F:\Dir-Personales\$cuenta -ItemType Directory
+#Recuperamos en la variable Acl la lista acl del directorio que se acaba de crear
 $Acl= Get-Acl "F:\Dir-Personales\$cuenta"
+#Se crea un objeto con los permisos para el usuario, en este caso, control Total
 $nueva_ACL = New-Object System.Security.AccessControl.FileSystemAccessRule("smr\$cuenta","FullControl","Allow")
+#Establecemos los permisos ($nueva_ACL) a la lista acl que se obtuvo anteriormente
 $Acl.SetAccessRule($nueva_ACL)
-Set-Acl "E:\Dir-Personales\$cuenta" $Acl
-
-
-https://www.jesusninoc.com/03/03/ejemplos-de-powershell-asignacion-de-permisos/
+#Establecemos la lista acl al directorio del usuario
+Set-Acl "F:\Dir-Personales\$cuenta" $Acl
