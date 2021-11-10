538190d69edc095847b0cd8f9fa20264abb8fda0
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-10 12:24:34 +0100

    Create permisos-PowerShell.ps1

---
+++ UD4/permisosPowerShell/permisos-PowerShell.ps1
@@ -0,0 +1,6 @@
+#Permisos de red. 
+#Crear un recurso compartido y asignar permisos al miso. Ejemplo: Asignar a la carpeta Publico los permisos de red siguientes:
+#Acceso total (FullAccess) al usuario administrador
+#Acceso en modo lectura (ReadAccess) para todos.
+
+New-SmbShare -Name Publico-RC -Path F:\Publico -FullAccess administrador -ReadAccess Everyone
