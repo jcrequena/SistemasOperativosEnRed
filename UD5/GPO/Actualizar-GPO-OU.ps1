c93d42eecff6a6f14afc5bb10f156553671fa8f9
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-14 08:46:31 +0100

    Update Actualizar-GPO-OU.ps1

--- UD5/GPO/Actualizar-GPO-OU.ps1
+++ UD5/GPO/Actualizar-GPO-OU.ps1
@@ -1,2 +1,2 @@
 #Actualizar GPOs en todos los equipos de una OU
-Get-ADComputer –filter * -Searchbase "OU=Equipos,DC=smr,DC=local" | foreach{ Invoke-GPUpdate –computer $_.name -force -RandomDelayInMinutes 0}
+Get-ADComputer –filter * -Searchbase "OU=Equipos-DepProduccion,OU=Dep-Produccion,OU=Departamentos,DC=smr,DC=local" | foreach{ Invoke-GPUpdate –computer $_.name -force -RandomDelayInMinutes 0}
