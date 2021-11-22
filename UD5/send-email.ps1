2865fd3c7f28b8b5ea351279c9d3136717ccfb8f
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-22 20:18:39 +0100

    Update send-email.ps1

--- UD5/send-email.ps1
+++ UD5/send-email.ps1
@@ -1,8 +1,8 @@
-$EmailPropio = "tuemaildegmail@gmail.com";
-$EmailDestino = "jrequenav@iesmiralcamp.org";
+$EmailPropio = "admin@smr.local";
+$EmailDestino = "juancarlos.requena@ieselcaminas.org"
 $Asunto = "El asunto del email"
 $Mensaje = "El cuerpo del mensaje"
-$ServidorSMTP = "smtp.gmail.com"
+$ServidorSMTP = "smtp.smr.local"
 $ClienteSMTP = New-Object Net.Mail.SmtpClient($ServidorSMTP, 587)
 $ClienteSMTP.EnableSsl = $true
 $ClienteSMTP.Credentials = New-Object System.Net.NetworkCredential("usuario", "contraseña");
@@ -10,5 +10,5 @@ $ClienteSMTP.Send($EmailPropio, $EmailDestino, $Asunto, $Mensaje)
 
 
 #Lo que hay que hacer en el Programador de tareas es:
-#Seleccionar como acción a realizar "Iniciar un programa" y en el siguiente paso escribir lo siguiente
-#powershell -file "C:\rutadelscript\send-email.ps1"
+#Seleccionar como acción a realizar "Iniciar un programa" y en el siguiente paso seleccionar el fichero a ejecutar
+
