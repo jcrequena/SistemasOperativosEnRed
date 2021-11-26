791e52887269faada8b46e52b4b05b6a1ef8c7c4
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-26 12:56:08 +0100

    Create sendEmail.ps1

---
+++ UD5/pista/sendEmail.ps1
@@ -0,0 +1,41 @@
+https://informaticamadridmayor.es/tips/send-mailmessage-envio-de-correos-electronicos-desde-powershell/
+
+$EmailDestinatario = "juancarlos.requena@ieselcaminas.org"
+$EmailEmisor = "admin@smr.local"
+$Asunto = "Prueba envío mail desde PowerShell"
+$CuerpoEnHTML = "Esto es una <b>prueba</b> de envío de correo electrónico desde PowerShell</a>"
+$SMTPServidor = "smtp.gmail.com"
+$RutaNombreFicheroAdjunto = "C:\Windows\System32\winevt\Logs\Security.evtx"
+$CodificacionCaracteres=[System.Text.Encoding]::UTF8
+try
+{
+  $SMTPMensaje = New-Object System.Net.Mail.MailMessage($EmailEmisor, $EmailDestinatario, $Asunto, $CuerpoEnHTML)
+  $SMTPAdjunto = New-Object System.Net.Mail.Attachment($RutaNombreFicheroAdjunto)
+  $SMTPMensaje.Attachments.Add($SMTPAdjunto)
+  $SMTPMensaje.IsBodyHtml = $true
+  $SMTPMensaje.BodyEncoding = $CodificacionCaracteres
+  $SMTPMensaje.SubjectEncoding = $CodificacionCaracteres
+  $SMTPCliente = New-Object Net.Mail.SmtpClient($SMTPServidor, 587)
+  $SMTPCliente.EnableSsl = $true
+  $password= Get-Credential
+  $SMTPCliente.Credentials = $password
+  
+  $mypasswd = ConvertTo-SecureString "smP@ssdw0rrd2" -AsPlainText -Force
+  #$mycreds = New-Object System.Management.Automation.PSCredential ("user1@woshub.com", $mypasswd)
+  New-Object System.Net.NetworkCredential($EmailEmisor, $mypasswd);
+  $SMTPCliente.Send($SMTPMensaje)
+  Write-Output "Mensaje enviado correctamente"
+}  
+catch
+{
+  Write-Error -Message "Error al enviar correo electrónico"
+}
+
+
+#Set-ExecutionPolicy RemoteSigned
+
+#Lo que hay que hacer en el Programador de tareas es:
+#Seleccionar como acción a realizar "Iniciar un programa" y en el siguiente paso seleccionar el fichero a ejecutar
+
+#Fichero de eventos/Logs del sistema sobre seguridad
+#C:\Windows\System32\winevt\Logs\Security.evtx
