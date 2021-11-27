5e2596fa7e1691f02022eb3a69a96e4cdb45580f
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-27 13:28:37 +0100

    Update sendEmail.ps1

--- UD5/pista/sendEmail.ps1
+++ UD5/pista/sendEmail.ps1
@@ -1,30 +1,47 @@
+#1. Comprimir el fichero a enviar
+#   Guardamos en 2 variables la ruta donde se encuentra el fichero de eventos de seguridad y donde vamos a generar el zip
+$rutaFicheroEventos = "C:\....."
+$rutaFicheroZip = "C:\Logs\Security.zip"
+
+#  En el parámetro -LiteralPath  le indicamos la ruta donde se encuentra el fichero de eventos de seguridad
+#  En el parámetro -DestinationPath le indicamos la ruta donde vamos a generar el zip del fichero de eventos de seguridad
+
+Compress-Archive -LiteralPath $rutaFicheroEventos -DestinationPath $rutaFicheroZip
+
+# 
+# Variable $accountFrom para almacenar la cuenta de email (cuenta de google) que se va a usar para enviar el mensaje
+# Variable $accountTo para almacenar la la cuenta de email a la que se quiere enviar el mensaje
+$accountFrom = "adm.smr.local@gmail.com"
+$accountTo = "juancarlos.requena@ieselcaminas.org"
 
 $MailMessage = @{
-  To = "admin@woshub.com"
-  Bcc = "jcrequena.fp@gmail.com"
-  From = "juancar.facebook@gmail.com"
-  Subject = "DC Server Report"
-  Body = "<h1>Welcome!</h1> <p><strong>Generated:</strong> $(Get-Date -Format g)</p>”
+  To = $accountTo
+  From = $accountFrom
+  Subject = "DC Server Report - Events Security"
+  Body = "<p>Se adjunta el fichero zip con los eventos de Seguridad con fecha:<strong> $(Get-Date -Format g)</strong></p>”
   Smtpserver = "smtp.gmail.com"
   Port = 587
   UseSsl = $true
   BodyAsHtml = $true
   Encoding = “UTF8”
-  Attachment = “C:adjunto.txt”
+  Attachment = $rutaFicheroZip
 }
 
 try
 {
- Send-MailMessage @MailMessage -Credential (Get-Credential)
- Write-Output "Mensaje enviado correctamente"
+  # Contraseña de la cuenta google que se va a usar para enviar el mensaje
+  $password = "Camina-100" 
+  #Convertimos la contraseña a un string seguro.
+  $secureStringPwd = ConvertTo-SecureString $password -AsPlainText -Force
+  #Construimos el objeto con las credenciales (cuenta de gmail y contraseña de la cuenta)
+  $credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $accountSource,$secureStringPwd
+  Send-MailMessage @MailMessage -Credential $credential
+  Write-Output "Mensaje enviado correctamente"
 }  
 catch
 {
   Write-Error -Message "Error al enviar correo electrónico"
 }
 
-#Lo que hay que hacer en el Programador de tareas es:
-#Seleccionar como acción a realizar "Iniciar un programa" y en el siguiente paso seleccionar el fichero a ejecutar
-Compress-Archive -LiteralPath C:\Logs\Security.evtx -DestinationPath C:\Logs\Security.zip
-#Fichero de eventos/Logs del sistema sobre seguridad
-#C:\Windows\System32\winevt\Logs\Security.evtx
+#Referencias:
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/send-mailmessage?view=powershell-7.2
