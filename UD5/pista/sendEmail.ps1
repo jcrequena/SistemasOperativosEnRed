c49883e5b9f42d22e3537459edfef13d972bc0eb
Juan Carlos <jcrequena.fp@gmail.com>
2021-11-27 12:04:36 +0100

    Update sendEmail.ps1

--- UD5/pista/sendEmail.ps1
+++ UD5/pista/sendEmail.ps1
@@ -25,6 +25,6 @@ catch
 
 #Lo que hay que hacer en el Programador de tareas es:
 #Seleccionar como acción a realizar "Iniciar un programa" y en el siguiente paso seleccionar el fichero a ejecutar
-
+Compress-Archive -LiteralPath C:\Logs\Security.evtx -DestinationPath C:\Logs\Security.zip
 #Fichero de eventos/Logs del sistema sobre seguridad
 #C:\Windows\System32\winevt\Logs\Security.evtx
