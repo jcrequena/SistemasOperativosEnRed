96542d26351d3094abe3fe770f9a546ca21b6de5
Juan Carlos <jcrequena.fp@gmail.com>
2023-08-24 17:14:57 +0200

    Delete UD3 directory

--- UD3/buscarEnActiveDirectory.ps1
+++
@@ -1,4 +0,0 @@
-#Listar los usuarios del dominio smr.local
- Get-ADUser -filter * -SearchBase "dc=smr,dc=local" | Select sAMAccountName
- 
- 
--- UD3/horasAcceso/logonHours.cmd
+++
@@ -1,5 +0,0 @@
-# L,M,X,J,v,S,D --> Días de la semana 
-#jcrequena es el usuario al que se le quiere establecer las horas de acceso
-#intervalo xam-xpm, donde x es un número entre 1 y 12.
-
-net user jcrequena /times:L,9am-3pm;M,9am-3pm;X,9am-3pm;J,9am-3pm;V,9am-3pm
