0ab25ac5dc0cdb6e653d3117f82fd760a3beb132
Juan Carlos <jcrequena.fp@gmail.com>
2023-10-26 12:38:00 +0200

    Update prueba.cmd

--- repositorio/Logo-Span /prueba.cmd
+++ repositorio/Logo-Span /prueba.cmd
@@ -1,7 +1,7 @@
 #Comandos cmd
 dsquery * -limit 20
 dsquery user -disabled
-dsquery user "cn=Users,dc=ciber,dc=mylocal"
+dsquery user "cn=Users,dc=logo-span,dc=com"
 dsquery computer
 set L
 #Comandos Powershell
