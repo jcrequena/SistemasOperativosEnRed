26151867ef3c3239356f73026c78349ef02d32ba
Juan Carlos <jcrequena.fp@gmail.com>
2023-11-02 16:34:16 +0100

    Update alta_Grupos.ps1

--- Powershell/scripts-PS/alta_Grupos.ps1
+++ Powershell/scripts-PS/alta_Grupos.ps1
@@ -4,12 +4,9 @@
 #Name:Path:Description:Category:Scope
 #SMR-GG-DepInformatica:OU=Dep-Informatica:Grupo global y de seguridad del Dep.Informatica:Security:Global
 #SMR-GG-DepLogistica:OU=Dep-Logistica:Grupo global y de seguridad del Dep.Logistica:Security:Global
-param($dominio,$sufijoDominio)
 
-#Componemos el Domain Component para el dominio que se pasa por parámetro
-# en este caso, el dominio es smr.local
-#Por lo que hay que componer dc=smr,dc=local
-$domainComponent="dc="+$dominio+",dc="+$sufijoDominio
+#Ponemos el Domain Component para el dominio en cuestión, que para este caso es smr.local
+$domain="dc=smr,dc=local"
 
 #
 #Creación de los grupos a partir de un fichero csv
@@ -19,7 +16,7 @@ $gruposCsv=Read-Host "Introduce el fichero csv de Grupos:"
 $fichero = import-csv -Path $gruposCsv -delimiter :
 foreach($linea in $fichero)
 {
-	$pathObject=$linea.Path+","+$domainComponent
+	$pathObject=$linea.Path
 	#Comprobamos si no existe el grupo antes de crearlo.
 	if ( !(Get-ADGroup -Filter { name -eq $linea.Name }) )
 	{
@@ -30,3 +27,6 @@ foreach($linea in $fichero)
 	}
 	else { Write-Host "El grupo $line.Name ya existe en el sistema"}
 }
+write-Host ""
+write-Host "Se han creado los grupos en el dominio $domain" -Fore green
+write-Host "" 
